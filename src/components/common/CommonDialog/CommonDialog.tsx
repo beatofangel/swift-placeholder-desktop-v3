1a96cc1ea4277f7daa413f3748fb0a9dbcdaeb33
Eric Wang <beatofangel@gmx.com>
2023-04-25 21:31:15 +0800

    rename

--- src/components/common/CommonDialog/CommonDialog.tsx
+++ src/components/common/CommonDialog/CommonDialog.tsx
@@ -8,7 +8,7 @@ import {
 import { VBtn } from "vuetify/lib/components/VBtn/index";
 import { VIcon } from "vuetify/lib/components/VIcon/index";
 import { VToolbar, VToolbarTitle } from "vuetify/lib/components/VToolbar/index";
-import { appName } from "@/composables";
+import { appName } from "@/composablesOld";
 import { VHover } from "vuetify/lib/components/VHover/index";
 import { isBoolean, isString } from "lodash";
 
